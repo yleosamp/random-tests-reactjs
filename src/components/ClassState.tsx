d36641dc02ce9bbbca218332afd265f03016db58
Leonardo Daitx <leonnardomelo3@gmail.com>
2023-10-19 21:24:43 -0300

    State in Class Components

--- src/components/ClassState.tsx
+++ src/components/ClassState.tsx
@@ -1,36 +1,34 @@
-import React from "react"
+import React from 'react'
 
 type Props = {
 }
 
 type State = {
-  color: string,
-  onOff: boolean
+	onOff: boolean,
+	color: string
 }
 
 class ClassState extends React.Component<Props, State> {
+	constructor(props: Props){
+		super(props)
+		this.state = {
+			onOff: false,
+			color: "green"
+		}
+	}
 
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      color: "yellow",
-      onOff: false
-    }
-  }
+	ligarLampada(){
+		this.setState({ onOff: !this.state.onOff })
+	}
 
-  onOff() {
-    this.setState({ onOff: !this.state.onOff })
-  }
-  
-  render(): React.ReactNode {
-    return(
-      <div>
-        <p>Cor da Lâmpada: { this.state.color } </p>
-        <p>A lâmpada está: { this.state.onOff ? "Ligada" : "Desligada" }</p>
-        <button onClick={ () => this.onOff() }>{ this.state.onOff ? "Desligar" : "Ligar" }</button>
-      </div>
-    )
-  }
+	render(): React.ReactNode{
+		return(
+			<div>
+				<p>A lâmpada está: { this.state.onOff ? 'Ligada' : 'Desligada' }</p>
+				<button onClick={ () => this.ligarLampada() }>{ this.state.onOff ? 'Desligar' : 'Ligar' }</button>
+			</div>
+		)
+	}
 }
 
 export default ClassState
